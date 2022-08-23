8edfdfd1a1ff49f1bfbce5a6100e1a300c8d2375
hypercross <helios.tnak@gmail.com>
2022-08-23 20:16:40 +0800

    2 files modified

--- pages/volleyball.tsx
+++ pages/volleyball.tsx
@@ -16,6 +16,8 @@ function VolleyballCard(props: { item: any }) {
   const { item } = props;
   if (item.variant === 'play') {
     return <PlayCard {...item} />;
+  } else if (item.variant === 'summary') {
+    return <SummaryCard {...item} />;
   }
   return null;
 }
@@ -41,8 +43,39 @@ function PlayCard(props: any) {
       <Layer className="catches">
         <Field code={props.Catches} all />
       </Layer>
+      <Layer className="name">
+        <Text>{props.Name}</Text>
+      </Layer>
     </Frame>
   );
+}
+
+function SummaryCard(props: any) {
+  if (props.side === 'back') {
+    return (
+      <Frame>
+        <Layer>
+          <Illust src={props.Illustration} />
+        </Layer>
+
+        <Layer className="name">
+          <Text>{props.Name}</Text>
+        </Layer>
+      </Frame>
+    );
+  }
+
+  const summary = props.summary;
+  const hints = Object.keys(summary).map((key) => {
+    const [_, f, p, d] = key.match(/(\w)-(\d)-(\d)/);
+    const n = summary[key];
+    return (
+      <Layer className={`summary field-${f} pos-${p} class-${d}`}>
+        <Text>{n}</Text>
+      </Layer>
+    );
+  });
+  return <Frame>{hints}</Frame>;
 }
 
 function Field(props: { code: string; all?: boolean }) {
@@ -66,7 +99,7 @@ function FieldBall(props: { code: string; all?: boolean }) {
       all.push(c);
     }
     return (
-      <FieldBallGroup>
+      <FieldBallGroup className={`ballgroup-${props.code}`}>
         {all.map((c) => (
           <FieldBall code={c} key={c} />
         ))}
@@ -77,6 +110,7 @@ function FieldBall(props: { code: string; all?: boolean }) {
 
 const Frame = el('div', 'volleyball');
 const Layer = el('div', 'layer');
+const Text = el('p', 'text');
 const Illust = el('img', 'illust');
 const FieldSvg = el('svg', 'playfield');
 const FieldBallGroup = el('g', 'ballgroup');
@@ -93,6 +127,29 @@ async function loadVolleyballCards() {
   const extra = await loadNotionDB(
     'https://nine-newsprint-c9d.notion.site/248a1ac98aa04b7695a2aaa9eb1e0af3?v=868764bf9803459caaff16184a2777da'
   );
+
+  for (const card of cards) {
+    if (card.variant !== 'summary') continue;
+
+    const hits = {} as { [key: string]: number };
+    for (const other of cards) {
+      if (other.variant !== 'play' || other.Name !== card.Name) continue;
+
+      for (let i = 0; i < 12; i++) {
+        const d = `${1 + Math.floor(i / 4)}`;
+        const p = i % 4;
+        if (other.Hits[p] === d) {
+          const key = `h-${p}-${d}`;
+          hits[key] = (hits[key] || 0) + 1;
+        }
+        if (other.Catches[p] === d) {
+          const key = `c-${p}-${d}`;
+          hits[key] = (hits[key] || 0) + 1;
+        }
+      }
+      card.summary = hits as any;
+    }
+  }
 
   const data = leftjoin('', cards, extra);
   return data;
